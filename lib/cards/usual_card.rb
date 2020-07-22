4c52010955566697c5c587ef36010067dc533972
vladveterok <vl.berezhnoi@gmail.com>
2020-07-22 14:34:57 +0300

    Update tax methods in cards -- replace tax_percent methods with constants

--- lib/cards/usual_card.rb
+++ lib/cards/usual_card.rb
@@ -23,14 +23,14 @@ class UsualCard < BasicCard
   end
 
   def withdraw_tax(amount:)
-    tax(amount, withdraw_tax_percent)
+    tax(amount, TAX_PERCENT[:withdraw], TAX_FIXED[:withdraw])
   end
 
   def put_tax(amount:)
-    tax(amount, put_tax_percent)
+    tax(amount, TAX_PERCENT[:put], TAX_FIXED[:put])
   end
 
   def sender_tax(amount:)
-    tax(amount, sender_tax_percent)
+    tax(amount, TAX_PERCENT[:sender], TAX_FIXED[:sender])
   end
 end
--- lib/cards/virtual_card.rb
+++ lib/cards/virtual_card.rb
@@ -21,4 +21,16 @@ class VirtualCard < BasicCard
   def number
     @number ||= generate_card_number
   end
+
+  def withdraw_tax(amount:)
+    tax(amount, TAX_PERCENT[:withdraw], TAX_FIXED[:withdraw])
+  end
+
+  def put_tax(amount:)
+    tax(amount, TAX_PERCENT[:put], TAX_FIXED[:put])
+  end
+
+  def sender_tax(amount:)
+    tax(amount, TAX_PERCENT[:sender], TAX_FIXED[:sender])
+  end
 end
